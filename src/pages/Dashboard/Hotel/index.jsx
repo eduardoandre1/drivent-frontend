ee5e4cd9cc6b9134ac45cf49a5c1b53077b2049b
Bia Luz <anabluz08@gmail.com>
2023-10-21 13:11:41 -0300

    feat: adiciona funcionalidade de busca por nome de usuário

--- src/pages/Dashboard/Hotel/index.jsx
+++ src/pages/Dashboard/Hotel/index.jsx
@@ -1,3 +1,34 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Typography from '@mui/material/Typography';
+import Button from '../../../components/Form/Button';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+const [escolheuQuarto, setEscolheuQuarto] = useState(true);
+
+
+return (
+<>
+<StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+
+<SubmitContainer>
+<Button type="submit" disabled={escolheuQuarto} > Reservar Quarto</Button>
+</SubmitContainer>
+</>
+);
 }
+
+const StyledTypography = styled(Typography)`
+margin-bottom: 20px!important;
+`;
+
+const SubmitContainer = styled.div`
+margin-top: 40px!important;
+width: 100%!important;
+
+> button {
+margin-top: 0 !important;
+color: #000000;
+
+}
+`;
